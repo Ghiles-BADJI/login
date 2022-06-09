ff1518bc0f9754a78f7e6fbf3bd58dc0d70413c8
Ghilès <ghiles.badji@gmail.com>
2022-06-09 15:30:35 +0200

    fix: matching password validator issue

--- src/app/signup/matching-password.validator.ts
+++ src/app/signup/matching-password.validator.ts
@@ -1,14 +1,16 @@
-import { AbstractControl, FormGroup, ValidatorFn } from "@angular/forms";
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function matchingPasswordValidator(passwordKey: string, passwordConfirmationKey: string): ValidatorFn {
-    return (control: AbstractControl) => {
-      const group = (control as FormGroup);
-      
-      const passwordInput = group.controls[passwordKey],
-          passwordConfirmationInput = group.controls[passwordConfirmationKey];
-      
-      const isDifferent = passwordInput.value === passwordConfirmationInput.value;
-      
-      return isDifferent ? { matchingPassword: true } : null;
-    }
+  return (control: AbstractControl) => {
+    const group = (control as FormGroup);
+
+    const passwordInput = group.controls[passwordKey],
+      passwordConfirmationInput = group.controls[passwordConfirmationKey];
+
+    const isDifferent = passwordInput.value !== passwordConfirmationInput.value;
+
+    const error: ValidationErrors = { matchingPassword: true }
+
+    return isDifferent ? error : null;
   }
+}
